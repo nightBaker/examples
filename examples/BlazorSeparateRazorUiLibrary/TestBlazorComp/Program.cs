edfe570348f9b37f3a8d02fad9fde9175aff4978
Ерасыл Шалабаев <yerassyl.shalabayev@outlook.com>
2020-04-14 20:30:41 +0600

    Example

---
+++ examples/BlazorSeparateRazorUiLibrary/TestBlazorComp/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using TestBlazorLib;
+
+namespace TestBlazorComp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
